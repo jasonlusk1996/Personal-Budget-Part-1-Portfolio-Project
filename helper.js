3de62f3df0c698cbf1fe5a9056a7cf56dc67dae0
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-03-18 16:15:58 -0500

    Create function to get envelope by id

---
+++ helper.js
@@ -0,0 +1,8 @@
+const express= require('express');
+const app = express();
+const envelopes = require('./envelopes.js');
+app.use(express.json());
+
+function getEnvelopeById(id) {
+    return envelopes.find(envelope => envelope.id === id)|| null;
+}
