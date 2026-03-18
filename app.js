c1d5b24e13316021915a519122ce07c67f84d055
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-03-18 13:50:10 -0500

    Add put request to make new envelope

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const envelopes = require('./envelopes.js');
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+});
+
+app.post('/envelopes/submit', (req, res) => {
+    const { name, budget } = req.body || {};
+    if(!name || !budget) {
+        return res.status(400).send('Name and budget are required');
+    }
+    envelopes.push({ id: envelopes.length + 1, name, budget, spent: 0 });
+    res.status(201).send('Envelope created successfully');
 });
 
 module.exports = app;
